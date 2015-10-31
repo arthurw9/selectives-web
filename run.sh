9f5cb14d6cda4a8754efc4bbe36648a7105eaf9c
Arthur Weinberger <arthurweinberger@gmail.com>
2015-10-31 12:40:41 -0700

    More merge changes from olga

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-/Applications/GoogleAppEngineLauncher.app/Contents/Resources/GoogleAppEngine-default.bundle/Contents/Resources/google_appengine/dev_appserver.py --datastore_path=/Users/MyMac/Sites/olga-selectives-web.db /Users/MyMac/Sites/olga-selectives-web &> /Users/MyMac/Sites/tmp/olga-selectives-web.log &
-echo "log file is at /Users/MyMac/Sites/tmp/olga-selectives-web.log &"
+~/programming/appengine/google_appengine/dev_appserver.py --datastore_path=/home/arthur/tmp/selectives-web.db ~/programming/appengine/selectives-web &> /tmp/selectives-web.log &
+echo "log file is at /tmp/selectives-web.log"
 echo "browse to http://localhost:8080"
