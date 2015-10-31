4377e9c63fcb7beecdeaa9e78ffc0e6fcd0fd0bf
Arthur Weinberger <arthurweinberger@gmail.com>
2015-10-31 13:55:17 -0700

    Add Sarah's changes to Olga's new page. Plus update some text in todo.txt

--- groups_students.py
+++ groups_students.py
@@ -75,7 +75,7 @@ class GroupsStudents(webapp2.RequestHandler):
 
     template_values = {
       'logout_url': logout_url,
-      'user' : auth.user,
+      'user_email' : auth.email,
       'institution' : institution,
       'session' : session,
       'message': message,
