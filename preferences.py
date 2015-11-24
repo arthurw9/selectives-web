cdb72f6ca094adfd7a02c57e9e9d48e2c4f14e7f
Sarah Moffatt <sarah.y.moffatt@gmail.com>
2015-11-23 18:35:38 -0800

    Hover text in schedule and preference shows different fields depending on whether the user is admin or student.

--- preferences.py
+++ preferences.py
@@ -83,7 +83,8 @@ class Preferences(webapp2.RequestHandler):
       class_id = str(c['id'])
       class_name = c['name']
       # TODO: add proper class_desc instead of just dumping the yaml
-      class_desc = yaml.dump(c, default_flow_style=False)
+      #class_desc = yaml.dump(c, default_flow_style=False)
+      class_desc = logic.GetHoverText(auth, c)
       classes_by_id[class_id] = {'name': class_name,
                                  'description': class_desc }
     if not classes_by_id:
